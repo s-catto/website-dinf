7029f3fcc638b36368b7b8388faafbc41510f998
sofi <sb.candiotto@gmail.com>
2026-08-03 08:35:19 -0300

    data de ferias 26-2

--- assets/scripts/ferias.js
+++ assets/scripts/ferias.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var ferias = new Date("Jul 5, 2026 00:00:00").getTime();
+var ferias = new Date("Dec 12, 2026 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
